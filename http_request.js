8c5a21d3a744c142609747b78d68be8ff79d2937
Gerald Haxhi <geraldhaxhi@outlook.com>
2020-02-03 16:12:32 +0100

    Http request example

---
+++ http_request.js
@@ -0,0 +1,45 @@
+const https = require('https');
+
+function run(iterations) {
+  const results = {};
+  
+  for (let i = 0; i < iterations; i++) {
+    results[i + 1] = { start: Date.now() };
+
+    https.request('https://nodejs.org/static/images/logo.svg', (res) => {
+      res.on('data', () => {});
+      res.on('end', () => {
+        results[i + 1].end = Date.now();
+      });
+    }).end();
+  }
+  
+  setTimeout(() => {
+  console.log(`\n
+  -----------| RESULT |-----------
+  `);
+
+  printIterationTimestamps(results);
+  console.log('\n');
+  }, 5000);
+}
+
+function printIterationTimestamps(result) {
+  let ref = null;
+  Object.keys(result).forEach((key) => {
+    if (key === '1') {
+      ref = result[key].start;
+    }
+    
+    const start = key === '1' ? 0 : result[key].start - ref;
+    const end = result[key].end - ref;
+
+    console.log(`  Iteration ${key} started at ${start} and ended at ${end}`);
+  });
+}
+
+if (process.argv && process.argv.length !== 3) {
+  throw new Error('Invalid args provided (Expected number of iterations)');
+} else {
+  run(parseInt(process.argv[2], 10));
+}
